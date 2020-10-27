0c4ad2319e8b487301fdc95b26feee53c12e5b94
wangwang-code <xiaogou200651@qq.com>
2020-10-27 19:27:21 +0800

    删除

--- src/main/java/me/tinkot/namecase/LoginEventListener.java
+++ src/main/java/me/tinkot/namecase/LoginEventListener.java
@@ -26,34 +26,32 @@ public class LoginEventListener implements Listener {
 	@EventHandler(priority = EventPriority.LOW)
 	public void onPlayerLogin(final PreLoginEvent event) {
 		event.registerIntent(plugin);
-		ProxyServer.getInstance().getScheduler().runAsync(plugin, new Runnable() {
-			public void run() {
+		ProxyServer.getInstance().getScheduler().runAsync(plugin, () -> {
 
-				Case reason = DEFAULT_CASE;
+			Case reason = DEFAULT_CASE;
 
-				String loginName = event.getConnection().getName();
+			String loginName = event.getConnection().getName();
 
 
-				if (!loginName.matches("[a-zA-Z0-9_]*")) {
-					reason = Case.ILLEGAL;
-				}
-
-				if (reason != Case.PASSED) {
-					String message;
-					if (reason == Case.ILLEGAL) {
-						message = config.getString("ILLEGAL_CHARS");
-						plugin.getLogger().info("玩家["+loginName+"]因带有非法字符而被踢出");
-					} else {
-						message = "unknown";
-
-					}
-					message = message.replace("\\n", "\n"); // hotfix for new line character
-
-					event.setCancelReason(new TextComponent(ChatColor.translateAlternateColorCodes('&', message)));
-					event.setCancelled(true);
-				}
-				event.completeIntent(plugin);
+			if (!loginName.matches("[a-zA-Z0-9_]*")) {
+				reason = Case.ILLEGAL;
 			}
+
+			if (reason != Case.PASSED) {
+				String message;
+				if (reason == Case.ILLEGAL) {
+					message = config.getString("ILLEGAL_CHARS");
+					plugin.getLogger().info("玩家["+loginName+"]因带有非法字符而被踢出");
+				} else {
+					message = "unknown";
+
+				}
+				message = message.replace("\\n", "\n"); // hotfix for new line character
+
+				event.setCancelReason(new TextComponent(ChatColor.translateAlternateColorCodes('&', message)));
+				event.setCancelled(true);
+			}
+			event.completeIntent(plugin);
 		});
 	}
 }
